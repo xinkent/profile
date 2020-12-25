4d21402bb76efec0f9933eb3504836db0025acd3
xinkent <xinkent1994@gmail.com>
2020-12-25 12:26:11 +0000

    add Skill Model

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'skills.apps.SkillsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
